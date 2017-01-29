efe9ae5f225cc3ff5547c33b157239b31c261255
Cody Yong <codytyong@utexas.edu>
2017-01-29 15:38:39 -0600

    Adding vertical movement to the ThirdPersonCamera. This basic camera controller does not account for wall collisions.

--- Assets/Scripts/ThirdPersonCamera.cs
+++ Assets/Scripts/ThirdPersonCamera.cs
@@ -3,7 +3,11 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class ThirdPersonCamera : MonoBehaviour {
-    public float rotateSpeed = 5;
+    public float rotateSpeed = 7.0f;
+    public float verticalRotateSpeed = 0.1f;
+
+    private float VERTICAL_THRESHOLD = 0.5f;
+
     private GameObject target;
 
 
@@ -18,6 +22,12 @@ public class ThirdPersonCamera : MonoBehaviour {
 
         // Match the rotation of the camera with the rotation of the target
         transform.rotation = target.transform.rotation;
+
+        // Get the Vertical Axes of the mouse to rotate the camera
+        float vertical = -Input.GetAxis("Mouse Y") * verticalRotateSpeed;
+
+        if (transform.localPosition.y + vertical <= VERTICAL_THRESHOLD && transform.localPosition.y + vertical >= -VERTICAL_THRESHOLD)
+            transform.localPosition = new Vector3(transform.localPosition.x, transform.localPosition.y + vertical, transform.localPosition.z);
 
         // Make the camera look at the target
         transform.LookAt(target.transform);
